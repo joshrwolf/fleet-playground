e97f19b857f7c2061d89defd26654c645f1ca1e8
Josh Wolf <josh@wolfs.io>
2020-09-26 00:32:50 -0600

    initial commit

---
+++ hack/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=0.3.0-beta6
+
+helm -n fleet-system install --create-namespace --wait \
+    fleet-crd https://github.com/rancher/fleet/releases/download/v${VERSION}/fleet-crd-${VERSION}.tgz
+
+helm -n fleet-system install --create-namespace --wait \
+    fleet https://github.com/rancher/fleet/releases/download/v${VERSION}/fleet-${VERSION}.tgz
