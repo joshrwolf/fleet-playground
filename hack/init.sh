b61eff57a8712a21a202dd08469e9f7e44cefd2e
Josh Wolf <josh@joshwolf.dev>
2020-10-05 09:42:57 -0600

    0.3.0

--- hack/init.sh
+++ hack/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.3.0-beta6
+VERSION=0.3.0
 
 helm -n fleet-system install --create-namespace --wait \
     fleet-crd https://github.com/rancher/fleet/releases/download/v${VERSION}/fleet-crd-${VERSION}.tgz
